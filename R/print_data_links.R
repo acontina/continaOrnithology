a638bb4c5576dddc272fb971e2b87c256c7bfe60
Andrea Contina <acontina@outlook.com>
2024-02-21 21:08:47 -0600

    Add files via upload

---
+++ R/print_data_links.R
@@ -0,0 +1,12 @@
+print_data_links <- function(data_links) {
+  data.link = "Use the link/s below to access the acoustic data stored in GitHub"
+  cat(green(data.link), "\n")
+  cat(red(data_links), "\n")
+}
+
+# Example usage
+data_links <- "https://github.com/acontina/continaOrnithology/tree/master/data"
+print_data_links(data_links)
+
+
+print_data_links(data_links)
